4ba8c4aa3c1e270a411868a05399c7d0fbd2da0a
Akito Iwakura <aki@asterleen.com>
2018-10-22 13:45:48 +0300

    Remove webkit's text input outlines in settings

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once ('engine/devdect.php');
 
 
 // Change me when css/js is changed
-define ('CLIENT_VERSION', 6);
+define ('CLIENT_VERSION', 7);
 
 
 $route = explode('/', $_GET['route']);
